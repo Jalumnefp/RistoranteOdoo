2d987cb5e07704b89783c49ce7a01484d47314d7
Jalumnefp <jauferper@alu.edu.gva.es>
2024-01-26 10:09:53 +0100

    Add models part2

--- models/models.py
+++ models/models.py
@@ -1,87 +1,88 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo.models import Model, AbstractModel
+from odoo.fields import Char, Text, Boolean, Integer, Date
 
 
-class Product(models.Model):
+class Product(Model):
     _name = 'ristorante.product'
     _description = 'Los platos del menú'
     
-    name = fields.Char(string="Name", required="True")
-    description = fields.Text(string="Description")
+    name = Char(string="Name", required="True")
+    description = Text(string="Description")
     #price = fields.Monetary(string="Price")
     
     #currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.company.currency_id.id)
 
 
-class Menu(models.Model):
+class Menu(Model):
     _name = 'ristorante.menu'
     _description = 'El menu del restaurante'
 
-    name = fields.Char(string="Name")
-    description = fields.Text(string="Description")
-    isVip = fields.Boolean()
+    name = Char(string="Name")
+    description = Text(string="Description")
+    isVip = Boolean()
     
     #products = fields.One2many(Products, string="Products")
     
     
-class Reserva(models.Model):
+class Reserva(Model):
     _name = 'ristorante.reserva'
     _description = 'La reserva de un cliente'
     
-    id = fields.Integer()
-    name = fields.Char()
+    id = Integer()
+    name = Char()
     #cliente = fields.One2one()
 
 
-class Persona(models.AbstractModel):
+class Persona(AbstractModel):
     _name = 'ristorante.persona'
     _description = 'Información general de personas'
     
-    dni = fields.Char(string="DNI")
-    name = fields.Char(string="Name")
-    birth_date = fields.Date(string="Date of birth")
-    phone = fields.Integer(string="Phone")
+    dni = Char(string="DNI")
+    name = Char(string="Name")
+    birth_date = Date(string="Date of birth")
+    phone = Integer(string="Phone")
     
     
-class Client(models.Model, Persona):
+class Client(Model, Persona):
     _name = 'ristorante.client'
     _description = 'Clientes del restaurante'
     
     #reservas
     
 
-class Empleado(models.AbstractModel, Persona):
+class Empleado(Persona):
     _name = 'ristorante.empleado'
     _description = 'Empleados del restaurante'
     
     #salary = fields.Monetary(string="Salary")
     
 
-class Cambrer(models.Model, Empleado):
+class Cambrer(Model, Empleado):
     _name = 'ristorante.cambrer'
     _description = 'Cambrers del restaurant'
     
     #id_ordre = fields.One2Many
     
     
-class Ordre(models.Model):
+class Ordre(Model):
     _name = 'ristorante.ordre'
     _description = 'Ordres dels clients del restaurant'
 
-    id = fields.Integer()
+    id = Integer()
     #taula = fields.One2one()
     #menu = fields.Many2one()
-    status = fields.Boolean()
+    status = Boolean()
     
     
-class Taula(models.Model):
+class Taula(Model):
     _name = 'ristorante.taula'
     _description = 'Taules del restaurant'
     
-    nom = fields.Char()
-    n_seients = fields.Integer()
-    isOcupat = fields.Boolean()
-    isVip = fields.Boolean()
-    isExterior = fields.Boolean()
+    nom = Char()
+    n_seients = Integer()
+    isOcupat = Boolean()
+    isVip = Boolean()
+    isExterior = Boolean()
     #reservada = fields.Boolean()
